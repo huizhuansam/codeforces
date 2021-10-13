bff89f49b03b8b85f594542c605f2def4d22c895
Siew Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2021-10-13 17:16:52 +0800

    add solution to 4A - Watermelon (#1)

---
+++ 4A - Watermelon/Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+    cin >> input;
+
+    if (input == 2 || input % 2 == 1) {
+        cout << "NO"; 
+    } else {
+        cout << "YES";
+    }
+
+    return 0;
+}
+
