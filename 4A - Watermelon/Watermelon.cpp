f83a70a5460ccffd32f1af0be99a807249dce395
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-10-13 18:10:18 +0800

    format file

--- 4A - Watermelon/Watermelon.cpp
+++ 4A - Watermelon/Watermelon.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int input;
     cin >> input;
 
-    if (input == 2 || input % 2 == 1) {
-        cout << "NO"; 
-    } else {
+    if (input == 2 || input % 2 == 1)
+    {
+        cout << "NO";
+    }
+    else
+    {
         cout << "YES";
     }
 
     return 0;
 }
-
