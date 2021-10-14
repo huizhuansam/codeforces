2bd52bb049fffa044a67385119ee8feb290092f8
Siew Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2021-10-14 23:42:23 +0800

    add solution (#2)

---
+++ 1519B - The Cake Is A Lie/TheCakeIsALie.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcases;
+    cin >> testcases;
+
+    for (int i = 0; i < testcases; i++)
+    {
+        int n, m, k;
+        cin >> n;
+        cin >> m;
+        cin >> k;
+        (n * m) - 1 == k ? cout << "YES" : cout << "NO";
+    }
+
+    return 0;
+}
