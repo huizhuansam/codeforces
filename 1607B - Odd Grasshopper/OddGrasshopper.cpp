f4d3fb90199eeb87e1e598b5acd79cf8ba4e1dcb
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-11-30 20:55:14 +0800

    add solutions

---
+++ 1607B - Odd Grasshopper/OddGrasshopper.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+long long int solve(long long int x, long long int n) {
+    return n % 4 == 0
+        ? x
+        : n % 4 == 1
+        ? (x % 2 == 0 ? x - n : x + n)
+        : n % 4 == 2
+        ? (x % 2 == 0 ? x + 1 : x - 1)
+        : (x % 2 == 0 ? x + n + 1 : x - n - 1);
+}
+
+int main()
+{
+    int t;
+    long long int x, n;
+    cin >> t;
+    while (t--) {
+        cin >> x >> n;
+        cout << solve(x, n) << "\n";
+    }
+    return 0;
+}
---
+++ 1614A - Divian and a Store/DivianAndAStore.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solve(int *arr, int n, int l, int r, int k) {
+    sort(arr, arr + n);
+    int res = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (k <= 0) break;
+        if (arr[i] >= l && arr[i] <= r && (k - arr[i] >= 0)) {
+            res++;
+            k -= arr[i];
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    int t, n, l, r, k;
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> l >> r >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        cout << solve(arr, n, l, r, k) << "\n";
+    }
+
+    return 0;
+}
